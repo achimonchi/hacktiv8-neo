9ab46ea610fc30ebf8df078092480481f7b9caf8
Reyhan Jovie Dwiputra <reyhanjovie01@gmail.com>
2022-11-21 10:14:15 +0700

    sesi 3

---
+++ sesi3/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sesi3/models"
+)
+
+func main() {
+	var user1 = models.User{
+		Id:       "U1",
+		Name:     "Hacktiv8",
+		Password: "hacktiv8",
+		Gender:   models.UserGender_MALE,
+	}
+
+	var userList = &models.UserList{
+		List: []*models.User{
+			&user1,
+		},
+	}
+
+	var garage1 = &models.Garage{
+		Id:   "G1",
+		Name: "Garage 1",
+		Coordinate: &models.GarageCoordinate{
+			Latitude:  23.001,
+			Longitude: 53.001,
+		},
+	}
+
+	var garageList1 = &models.GarageList{
+		List: []*models.Garage{
+			garage1,
+			garage1,
+		},
+	}
+
+	var garageListByUser = &models.GarageListByUser{
+		List: map[string]*models.GarageList{
+			user1.Id: garageList1,
+		},
+	}
+
+	fmt.Printf("User List :%+v\n", userList)
+	fmt.Printf("Garage List :%+v\n", garageListByUser)
+}
