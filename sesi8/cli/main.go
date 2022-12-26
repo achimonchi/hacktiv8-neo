b095c3bb3afdc1af73933131a5d0ee3b2fcedd78
Reyhan Jovie Dwiputra <reyhanjovie01@gmail.com>
2022-12-26 11:28:13 +0700

    jawaban latihan

--- sesi8/cli/main.go
+++ sesi8/cli/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strconv"
+	"strings"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 )
@@ -12,7 +15,7 @@ var (
 	app = kingpin.New("App", "Simple App")
 
 	add             = app.Command("add", "Add User")
-	addFlagOverride = add.Flag("override", "Override user").Short('o').String()
+	addFlagOverride = add.Flag("override", "Override user").Short('o').Default("false").String()
 	addArgUser      = add.Arg("user", "username").Required().String()
 
 	update           = app.Command("update", "Update User")
@@ -20,7 +23,7 @@ var (
 	updateArgNewUser = update.Arg("new", "new username").Required().String()
 
 	delete          = app.Command("delete", "Delete User")
-	deleteFlagForce = delete.Flag("force", "Force delete user").Short('f').String()
+	deleteFlagForce = delete.Flag("force", "Force delete user").Default("false").Short('f').String()
 	deleteArgUser   = delete.Arg("user", "username").Required().String()
 
 	get         = app.Command("get", "Get user")
@@ -30,7 +33,31 @@ var (
 
 var Users []string = []string{}
 
+var filePath = "./cli/data.json"
+
 func main() {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	var datas = map[string][]string{}
+	err = json.Unmarshal(data, &datas)
+	if err != nil {
+		panic(err)
+	}
+
+	Users = datas["users"]
+
+	updateFunc := func(datas map[string][]string, newData []string, key string) error {
+		datas[key] = newData
+		dataJson, err := json.Marshal(datas)
+		if err != nil {
+			return err
+		}
+		return ioutil.WriteFile(filePath, dataJson, 0644)
+	}
+
 	add.Action(func(pc *kingpin.ParseContext) error {
 		user := *addArgUser
 		override := *addFlagOverride
@@ -42,14 +69,32 @@ func main() {
 
 		fmt.Printf("adding user %s, override %t \n", user, overrideBool)
 
-		return nil
+		if overrideBool {
+			index, isExist := findUser(user)
+			if isExist {
+				Users[index] = user
+				return updateFunc(datas, Users, "users")
+			} else {
+				fmt.Printf("user %s not exists. so will continue to add it \n", user)
+			}
+		}
+
+		Users = append(Users, user)
+		return updateFunc(datas, Users, "users")
 	})
 	update.Action(func(pc *kingpin.ParseContext) error {
 		old := *updateArgOldUser
 		new := *updateArgNewUser
 
+		index, isExist := findUser(old)
+		if !isExist {
+			return fmt.Errorf("username %s not exist", old)
+		}
+
+		Users[index] = new
+
 		fmt.Printf("updating %s to %s\n", old, new)
-		return nil
+		return updateFunc(datas, Users, "users")
 	})
 	delete.Action(func(pc *kingpin.ParseContext) error {
 		user := *deleteArgUser
@@ -60,9 +105,45 @@ func main() {
 			return err
 		}
 
+		index, isExist := findUser(user)
+		if !isExist {
+			return fmt.Errorf("username %s not exist", user)
+		}
+
 		fmt.Printf("delete user with username %s with force is %t\n", user, forceBool)
+		Users = append(Users[:index], Users[index+1:]...)
+
+		return updateFunc(datas, Users, "users")
+	})
+	get.Action(func(pc *kingpin.ParseContext) error {
+		all := *getFlagAll
+		username := *getFlagUser
+
+		if all == "all" {
+			fmt.Printf("%s GET ALL USERS %s\n", strings.Repeat("=", 10), strings.Repeat("=", 10))
+			for _, user := range Users {
+				fmt.Println("User :", user)
+			}
+			fmt.Printf("%s\n", strings.Repeat("=", 35))
+		} else {
+			index, isExist := findUser(username)
+			if !isExist {
+				return fmt.Errorf("username %s not exist", username)
+			}
+			fmt.Println("User :", Users[index])
+		}
 		return nil
 	})
 
 	kingpin.MustParse(app.Parse(os.Args[1:]))
+}
+
+func findUser(username string) (int, bool) {
+	for i, user := range Users {
+		if user == username {
+			return i, true
+		}
+	}
+
+	return 0, false
 }
